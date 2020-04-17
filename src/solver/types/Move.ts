8e9d5b8696efd0cc2cb4a768aebf9e09d28baf74
佐藤大介 <satoudaisuke@satoudaisuke-no-MacBook-Pro.local>
2020-04-17 18:28:33 +0900

    [add] calculatePossibleMoves(BFS), [fix] Move type

--- src/solver/types/Move.ts
+++ src/solver/types/Move.ts
@@ -1,8 +1,9 @@
 import { Cell } from './Cell';
+import Block from './Block';
 
 type Vector = Cell;
 export type Move = {
-  ancher: Cell;
+  block: Block; // block is coordinates(座標) before movement
   direction: Vector;
 };
 
