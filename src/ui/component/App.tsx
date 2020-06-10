0219b569f7b1e224551edbf49540ea12e8ac6458
佐藤大介 <satoudaisuke@satoudaisuke-no-MacBook-Pro.local>
2020-06-10 19:27:47 +0900

    [add] ghost_move

--- src/ui/component/App.tsx
+++ src/ui/component/App.tsx
@@ -30,7 +30,11 @@ const App: FC<{}> = () => {
 
       <BoardView
         board={questions.basic1}
-        ghost_blocks={[{ type: 'horizontal', ancher: new Cell(1, 4) }]}
+        translucent_blocks={[{ type: 'horizontal', ancher: new Cell(1, 4) }]}
+        ghost_move={{
+          block: questions.basic1.blocks[5],
+          direction: new Cell(1, 0),
+        }}
       />
       <BoardView board={questions.easy1} />
       <BoardView board={questions.hard1} />
--- src/ui/component/BoardView.tsx
+++ src/ui/component/BoardView.tsx
@@ -4,57 +4,23 @@ import Move from '../../solver/types/Move';
 import Block from '../../solver/types/Block';
 import Cell from '../../solver/types/Cell';
 import styled from '@emotion/styled';
-
-const CELL_SIZE_PX = 50;
-const BLOCK_MARGIN_PX = 10;
-const BLOCK_COLORS: { [key in Block['type']]: string } = {
-  dot: '#35d0ba',
-  vertical: '#ffcd3c',
-  horizontal: '#ff9234',
-  target: '#d92027',
-};
-const BlockView: FC<{
-  block: Block;
-  is_ghost?: boolean;
-}> = ({ block, is_ghost }) => {
-  const type = block.type;
-  const width_px =
-    type === 'dot' || type === 'vertical'
-      ? CELL_SIZE_PX
-      : CELL_SIZE_PX * 2 + BLOCK_MARGIN_PX;
-  const height_px =
-    type === 'dot' || type === 'horizontal'
-      ? CELL_SIZE_PX
-      : CELL_SIZE_PX * 2 + BLOCK_MARGIN_PX;
-
-  const StyledBlock = styled.div`
-    position: absolute;
-    top: ${BLOCK_MARGIN_PX +
-    (BLOCK_MARGIN_PX + CELL_SIZE_PX) * block.ancher.y}px;
-    left: ${BLOCK_MARGIN_PX +
-    (BLOCK_MARGIN_PX + CELL_SIZE_PX) * block.ancher.x}px;
-    width: ${width_px}px;
-    height: ${height_px}px;
-    background: ${BLOCK_COLORS[type]};
-    box-shadow: 0 3px 5px #0002, 0 1px 5px #0002;
-    border-radius: 3px;
-    opacity: ${is_ghost ? 0.5 : 1};
-  `;
-  return <StyledBlock />;
-};
+import BlockView from './BlockView';
 
 const BoardView: FC<{
   board: Board;
-  ghost_blocks?: Block[];
+  translucent_blocks?: Block[];
   ghost_move?: Move;
   onCellClick?: (cell: Cell) => void;
   onCellEnter?: (cell: Cell) => void;
   onCellLeave?: (cell: Cell) => void;
-}> = ({ board, ghost_blocks }) => {
+}> = ({ board, translucent_blocks, ghost_move }) => {
   const getBlockKey = (block: Block): string => block.type + block.ancher;
   const blocks = board.blocks.map((x) => (
     <BlockView block={x} key={getBlockKey(x)} />
   ));
+
+  const CELL_SIZE_PX = 50;
+  const BLOCK_MARGIN_PX = 10;
 
   const width_px =
     BLOCK_MARGIN_PX + (CELL_SIZE_PX + BLOCK_MARGIN_PX) * Board.WIDTH;
@@ -81,11 +47,17 @@ const BoardView: FC<{
   return (
     <StyledBoard>
       {blocks}
-      {ghost_blocks
-        ? ghost_blocks.map((x) => (
-            <BlockView block={x} is_ghost={true} key={getBlockKey(x)} />
+      {translucent_blocks
+        ? translucent_blocks.map((x) => (
+            <BlockView block={x} is_translucent={true} key={getBlockKey(x)} />
           ))
         : null}
+      {ghost_move ? (
+        <BlockView
+          block={ghost_move.block}
+          move_animation={ghost_move?.direction}
+        />
+      ) : null}
     </StyledBoard>
   );
 };
