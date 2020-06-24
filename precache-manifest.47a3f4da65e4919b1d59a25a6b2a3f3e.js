1d87f78de6094f24924dc33ce6878a92a221cc7b
dsukesato <dsukesato@users.noreply.github.com>
2020-06-24 08:20:30 +0000

    deploy: bf18b8ba109cf3b0fccae9b6a1ca3db3ecd0beb0

---
+++ precache-manifest.47a3f4da65e4919b1d59a25a6b2a3f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947cce85ce653a8ded343802c1feae5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34c738239ec8714250ce",
+    "url": "./static/js/2.de256e12.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.de256e12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9575752887cb698ad502",
+    "url": "./static/js/main.f6e0a5ce.chunk.js"
+  },
+  {
+    "revision": "0b2eb14a375db1b0f42f",
+    "url": "./static/js/runtime-main.3a1012d3.js"
+  }
+]);
